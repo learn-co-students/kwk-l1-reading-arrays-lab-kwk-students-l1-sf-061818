e8b5a471694262ef788286512a7ee2fc2a3e085f
ismillet <isabella@duckler.com>
2018-06-19 23:24:10 +0000

    Done.

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,4 +1,7 @@
-STUDENT_NAMES = [
+
+#six total students 
+def first_student_by_index
+  student_names= [
   "Adele",
   "Beyoncé",
   "Cardi B",
@@ -6,27 +9,72 @@ STUDENT_NAMES = [
   "Nicki Minaj",
   "Rihanna"
 ]
-
-def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  puts student_names[0]
 end
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  student_names= [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+  puts student_names[3]
 end
 
 def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  student_names= [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+  puts student_names[5]
 end
 
 def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
+  student_names= [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+  puts student_names.first 
 end
 
 def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
+  student_names= [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+  puts student_names.last
 end
 
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+  student_names= [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+  puts student_names[0..2]
 end
+
+first_student_by_index
+fourth_student_by_index
+last_student_by_index
+first_student_by_method
+last_student_by_method
+first_second_and_third_students
